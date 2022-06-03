1aaf322a05cea39f2a74d23083d2060334c57e12
Agnes Yim <agnyim.hk@gmail.com>
2022-06-03 14:49:22 +0200

    PlantItem component

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,5 +1,6 @@
 import '../styles/ShoppingList.css';
 import { plantList } from '../datas/plantList';
+import PlantItem from './PlantItems';
 
 function ShoppingList() {
 	const categories = plantList.reduce(
@@ -15,10 +16,14 @@ function ShoppingList() {
 				))}
 			</ul>
 			<ul className='lmj-plant-list'>
-				{plantList.map((plant) => (
-					<li key={plant.id} className='lmj-plant-item'>
-                        {plant.name}{plant.isSpecialOffer ? <div className='lmj-sales'>Soldes</div> : null}
-                    </li>
+				{plantList.map(({ id, cover, name, water, light }) => (
+					<PlantItem 
+						id = {id}
+						cover = {cover}
+						name = {name}
+						water = {water}
+						light = {light}
+					/>
 				))}
 			</ul>
 		</div>
